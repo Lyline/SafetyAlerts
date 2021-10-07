3aa0f1989ff62bc8b39d2b497193d4c3a5265a60
Lyline <greg.quesne@gmail.com>
2021-10-07 09:32:04 +0200

    Impl - logs in FireStationController

--- src/main/java/fr/lyline/SafetyAlerts/controller/FireStationController.java
+++ src/main/java/fr/lyline/SafetyAlerts/controller/FireStationController.java
@@ -2,6 +2,8 @@ package fr.lyline.SafetyAlerts.controller;
 
 import fr.lyline.SafetyAlerts.model.FireStation;
 import fr.lyline.SafetyAlerts.service.FireStationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +17,21 @@ public class FireStationController {
   @Autowired
   FireStationService service;
 
+  Logger logger = LogManager.getLogger(FireStationController.class);
+
   @GetMapping("/firestations")
   public ResponseEntity<List<Integer>> getFireStations() {
     List<Integer> response = service.getAllFireStations();
 
     if (!response.isEmpty()) {
+      logger.info("GET /firestations : " + response.size() + " station(s) - Status 200");
+      logger.info(response);
       return new ResponseEntity<>(response, HttpStatus.OK);
-    } else return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    } else {
+      logger.warn("GET /firestations : No Data found - Status 404");
+      logger.info(response);
+      return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
   }
 
   @GetMapping("/firestations/{address}")
@@ -29,18 +39,36 @@ public class FireStationController {
     List<Integer> response = service.getFireStation(address);
 
     if (!response.isEmpty()) {
+      logger.info("GET /firestations/" + address + " - Status 200");
+      logger.info(response);
       return new ResponseEntity<>(response, HttpStatus.OK);
-    } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } else {
+      logger.warn("GET /firestations/" + address + " - Status 404");
+      logger.info("null");
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
   }
 
   @PostMapping("/firestations")
   public ResponseEntity addFireStation(@RequestBody FireStation fireStation) {
     List<Integer> response = service.getFireStation(fireStation.getAddress());
-
+    if (fireStation.getStation() == null | fireStation.getAddress() == null) {
+      logger.warn("POST /firestations " + fireStation.getStation() + "-" + fireStation.getAddress() +
+          " - Incomplete informations for creation - Status 404");
+      logger.info(response);
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
     if (!response.contains(fireStation.getStation())) {
       service.addFireStation(fireStation);
-      return new ResponseEntity<>(HttpStatus.CREATED);
-    } else return new ResponseEntity<>(HttpStatus.CONFLICT);
+      logger.info("POST /firestations : " + fireStation.getStation() + "-" + fireStation.getAddress() + " - Status 201");
+      logger.info(fireStation.toString());
+      return new ResponseEntity<>(fireStation, HttpStatus.CREATED);
+    } else {
+      logger.warn("POST /firestations : " + fireStation.getStation() + "-" + fireStation.getAddress() +
+          " - Station already exist - Status 409");
+      logger.info("null");
+      return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+    }
   }
 
   @PatchMapping("/firestations/{oldFS}-{address}")
@@ -50,8 +78,14 @@ public class FireStationController {
     boolean result = service.updateFireStation(oldFS, address, fireStation);
 
     if (result) {
+      logger.info("PATCH /firestations/" + fireStation.getStation() + "-" + fireStation.getAddress() + " - Status 200");
+      logger.info("Exist : " + oldFS + "-" + address + "\nUpdate : " + fireStation);
       return new ResponseEntity<>(fireStation, HttpStatus.OK);
-    } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } else {
+      logger.warn("PATCH /firestations/" + oldFS + "-" + address + " - Status 404");
+      logger.info("null");
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
   }
 
   @DeleteMapping("/firestations/{stationNumber}-{address}")
@@ -61,7 +95,13 @@ public class FireStationController {
 
     if (response.contains(Integer.valueOf(stationNumber))) {
       service.removeFireStation(stationNumber, address);
+      logger.info("DELETE /firestations/" + stationNumber + "-" + address + " - Status 200");
+      logger.info("Exist : " + stationNumber + "-" + address);
       return new ResponseEntity(HttpStatus.OK);
-    } else return new ResponseEntity(HttpStatus.NOT_FOUND);
+    } else {
+      logger.warn("DELETE /firestations/" + stationNumber + "-" + address + " - Fire station not exist - Status 404");
+      logger.info("null");
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
   }
 }
